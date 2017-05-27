4cd0e5b57b9c37c4ff2732635cb1f279dd3a5cb8
lingco <632135186@qq.com>
2017-05-27 14:16:09 +0800

    测试提交

--- src/main/java/com/daqsoft/config/WebSecurityConfig.java
+++ src/main/java/com/daqsoft/config/WebSecurityConfig.java
@@ -41,7 +41,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
                 .formLogin()
                 .loginPage("/login")
                 .failureUrl("/login?error")
-                .permitAll() //登录页面用户任意访问
+                .permitAll() //登录页面用户任意访问，不需要权限的页面
                 .and()
                 .logout().permitAll(); //注销行为任意访问
         http.addFilterBefore(myFilterSecurityInterceptor, FilterSecurityInterceptor.class);
