068e592782f5fff3fff0b7b55590fd7f5dda7a78
lingco <632135186@qq.com>
2017-05-27 14:45:41 +0800

    测试

---
+++ src/main/java/com/daqsoft/controller/TestController.java
@@ -0,0 +1,11 @@
+package main.java.com.daqsoft.controller;
+
+/**
+ * @author tanggm
+ * @date 2017-05-27 14:45
+ */
+public class TestController {
+    public String get(){
+        return null;
+    }
+}
